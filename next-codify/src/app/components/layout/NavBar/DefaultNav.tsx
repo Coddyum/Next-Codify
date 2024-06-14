69bb0ef774eb0f6431aa21321cdea8f1309a10c4
Maxence <coddyumpro@gmail.com>
2024-06-14 04:13:07 +0200

    i tailwindcss-animated

--- next-codify/src/app/components/layout/NavBar/DefaultNav.tsx
+++ next-codify/src/app/components/layout/NavBar/DefaultNav.tsx
@@ -2,14 +2,17 @@ import DefaultBtn from "../../common/Button/DefaultBtn";
 import Logo from "../../common/Logo/logo";
 
 const DefaultNav = () => {
-    return(
+    return (
         <>
-            <div className="max-w-screen-xl h-16 bg-transparent mx-auto flex items-center justify-center">
-                <Logo/>
-                <DefaultBtn text={"Get Stated"}/>
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex justify-between items-center py-3">
+                    <Logo />
+                    <DefaultBtn text={"Get Started"} />
+                </div>
             </div>
+            <hr className="border-or-300" />
         </>
-    )
-}
+    );
+};
 
 export default DefaultNav;
--- next-codify/tailwind.config.ts
+++ next-codify/tailwind.config.ts
@@ -28,6 +28,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 };
 export default config;
