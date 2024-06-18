9dd46d9cc0c251f941cbe50eef7074c06c50af2d
Maxence <coddyumpro@gmail.com>
2024-06-18 12:37:52 +0200

    update

--- next-codify/src/app/components/layout/SignIn/SignIn.tsx
+++ next-codify/src/app/components/layout/SignIn/SignIn.tsx
@@ -1,11 +1,9 @@
 "use client";
 
 import LoginNav from "../home/NavBar/LoginNav";
-import Link from "next/link";
-import DefaultBtn from "../../common/Button/DefaultBtn";
 import useClientRouter from "../../../hooks/useClientRouter";
 
-export default function SignUp() {
+export default function SignIn() {
     const router = useClientRouter();
 
     const validateEmail = (email: string): boolean => {
@@ -13,26 +11,13 @@ export default function SignUp() {
         return emailRegex.test(email);
     };
 
-    const validatePassword = (password: string): boolean => {
-        const passwordRegex =
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return passwordRegex.test(password);
-    };
-
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const form = event.target as HTMLFormElement;
-        const email = (form.elements.namedItem("Email") as HTMLInputElement)
-            .value;
-        const pseudo = (form.elements.namedItem("Pseudo") as HTMLInputElement)
-            .value;
-        const password = (form.elements.namedItem("pwd") as HTMLInputElement)
-            .value;
-        const confirmPassword = (
-            form.elements.namedItem("confirmPwd") as HTMLInputElement
-        ).value;
+        const email = (form.elements.namedItem("Email") as HTMLInputElement).value;
+        const password = (form.elements.namedItem("pwd") as HTMLInputElement).value;
 
-        if (!email || !pseudo || !password || !confirmPassword) {
+        if (!email || !password) {
             alert("Tous les champs doivent être remplis.");
             return;
         }
@@ -42,26 +27,14 @@ export default function SignUp() {
             return;
         }
 
-        if (!validatePassword(password)) {
-            alert(
-                "Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, un chiffre et un caractère spécial."
-            );
-            return;
-        }
-
-        if (password !== confirmPassword) {
-            alert("Les mots de passe ne correspondent pas.");
-            return;
-        }
-
         if (router) {
-            router.push("./RoadMapSelect");
+            router.push('./RoadMapSelect');
         }
     };
 
     return (
         <>
-            <div className="h-screen w-screen bg-gradient-to-br to-red-200/70 via-transparent from-transparent">
+            <div className="h-screen w-screen bg-gradient-to-br to-or-300/80 via-transparent from-transparent">
                 <nav>
                     <LoginNav />
                 </nav>
@@ -69,7 +42,7 @@ export default function SignUp() {
                     <form onSubmit={handleSubmit}>
                         <div className="border-2 border-or-300 w-fit rounded-2xl mx-auto p-3">
                             <h1 className="text-white text-2xl font-medium py-2 px-36 mt-2">
-                                Sign Up
+                                Sign In
                             </h1>
                             <div className="mt-10">
                                 <label htmlFor="Email">
@@ -82,16 +55,6 @@ export default function SignUp() {
                                         placeholder="Email"
                                     />
                                 </label>
-                                <label htmlFor="Pseudo">
-                                    <input
-                                        className="flex py-2 px-20 mb-4 border-2 border-or-300 rounded-xl bg-transparent mx-auto text-center text-white"
-                                        type="text"
-                                        name="Pseudo"
-                                        id="Pseudo"
-                                        required
-                                        placeholder="Pseudo"
-                                    />
-                                </label>
                                 <label htmlFor="pwd">
                                     <input
                                         className="flex py-2 px-20 mb-4 border-2 border-or-300 rounded-xl bg-transparent mx-auto text-center text-white"
@@ -102,25 +65,12 @@ export default function SignUp() {
                                         placeholder="Password"
                                     />
                                 </label>
-                                <label htmlFor="confirmPwd">
-                                    <input
-                                        className="flex py-2 px-20 mb-4 border-2 border-or-300 rounded-xl bg-transparent mx-auto text-center text-white"
-                                        type="password"
-                                        name="confirmPwd"
-                                        id="confirmPwd"
-                                        required
-                                        placeholder="Confirm Password"
-                                    />
-                                </label>
                                 <div className="flex items-center justify-center my-8 gap-3">
                                     <button
                                         type="submit"
                                         className="w-auto h-auto p-2 ml-2 flex items-center justify-center border-2 border-or-300 rounded-lg text-white font-medium text-sm transition duration-500 ease-out hover:bg-or-300 hover:text-slate-950">
-                                        Sign Up
+                                        Sign In
                                     </button>
-                                    <Link href="/pages/signin">
-                                        <DefaultBtn text={"Sign In"} />
-                                    </Link>
                                 </div>
                             </div>
                         </div>
---
+++ next-codify/src/app/pages/signin/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SignIn from "@/app/components/layout/SignIn/signin";
+import "../../globals.css";
+
+export default function signup() {
+    return (
+        <>
+            <SignIn/>
+        </>
+    );
+}
