4fb715a1bcd399e3b06a1ec55e85ee54110979da
Maxence <coddyumpro@gmail.com>
2024-06-20 21:32:32 +0200

    up

--- next-codify/src/app/components/layout/RoadMapCard/RoadMapCards.tsx
+++ next-codify/src/app/components/layout/RoadMapCard/RoadMapCards.tsx
@@ -8,13 +8,13 @@ export default function RoadMapCards() {
                     <div
                         key={data.id}
                         title="container"
-                        className="w-64 h-80 rounded-2xl bg-slate-800 text-white font-jet">
+                        className="w-72 h-96 rounded-2xl bg-slate-800 text-white font-jet">
                         <img
                             src={data.image}
                             alt=""
-                            className="w-56 h-36 object-cover mx-auto mt-4 rounded-xl"
+                            className="w-64 h-40 object-cover mx-auto mt-4 rounded-xl"
                         />
-                        <div className="w-56 mx-auto mt-3 bg-green-600">
+                        <div className="w-64 mx-auto mt-3 bg-green-600">
                             <h2 className="text-white font-medium">{data.title}</h2>
                         </div>
                     </div>
