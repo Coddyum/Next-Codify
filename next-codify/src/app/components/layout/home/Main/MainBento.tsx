71bac2ccb6537a37ed6985c98c72384058a86a4b
Maxence <coddyumpro@gmail.com>
2024-06-22 19:59:35 +0200

    up

--- next-codify/src/app/components/layout/home/Main/MainBento.tsx
+++ next-codify/src/app/components/layout/home/Main/MainBento.tsx
@@ -7,29 +7,29 @@ export default function Main() {
     return (
         <>
             <Cursor />
-            <div className="w-screen h-auto">
+            <div className="w-screen h-full pb-36 bg-gradient-to-tr to-or-300 via-transparent from-transparent">
                 <div
                     className="max-w-screen-xl mx-auto flex gap-5 flex-wrap container mb-24"
                     style={{ "--clr": "#ecb99d" } as React.CSSProperties}>
                     <div className="flex flex-col gap-5 ml-5">
                         <div className="flex gap-5">
-                            <div className="card w-300 h-300 bg-transparent border border-gray-800 rounded-3xl flex items-center justify-center">
+                            <div className="card w-300 h-300 bg-transparent rounded-3xl flex items-center justify-center">
                                 <div className="w-[294px] h-[294px] bg-bunker-950 rounded-3xl z-20"></div>
                             </div>
-                            <div className="card2 card w-600 h-300 bg-transparent border border-gray-800 rounded-3xl flex items-center justify-center">
+                            <div className="card2 card w-600 h-300 bg-transparent rounded-3xl flex items-center justify-center">
                                 <div className="w-[594px] h-[294px] bg-bunker-950 rounded-3xl z-20"></div>
                             </div>
                         </div>
                         <div className="flex gap-5">
-                            <div className="card2 card w-600 h-300 bg-transparent border border-gray-800 rounded-3xl flex items-center justify-center">
+                            <div className="card2 card w-600 h-300 bg-transparent rounded-3xl flex items-center justify-center">
                                 <div className="w-[594px] h-[294px] bg-bunker-950 rounded-3xl z-20"></div>
                             </div>
-                            <div className="card w-300 h-300 bg-transparent border border-gray-800 rounded-3xl flex items-center justify-center">
+                            <div className="card w-300 h-300 bg-transparent rounded-3xl flex items-center justify-center">
                                 <div className="w-[294px] h-[294px] bg-bunker-950 rounded-3xl z-20"></div>
                             </div>
                         </div>
                     </div>
-                    <div className="card3 card w-300 h-616 bg-transparent border border-gray-800 rounded-3xl flex items-center justify-center">
+                    <div className="card3 card w-300 h-616 bg-transparent rounded-3xl flex items-center justify-center">
                         <div className="w-[294px] h-[610px] bg-bunker-950 rounded-3xl z-20"></div>
                     </div>
                 </div>
