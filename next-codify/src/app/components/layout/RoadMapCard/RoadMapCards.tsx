01cf1bcaaa5cdd7898eca754f48a957158265128
Maxence <coddyumpro@gmail.com>
2024-06-26 12:27:44 +0200

    update card

--- next-codify/src/app/components/layout/RoadMapCard/RoadMapCards.tsx
+++ next-codify/src/app/components/layout/RoadMapCard/RoadMapCards.tsx
@@ -1,24 +1,36 @@
 import CardData from "../../../../../public/data/RoadMapCard.json";
 import EnRollBtn from "../../common/Button/EnRollBtn";
+import { HeartIcon } from '@heroicons/react/24/outline'
 
 export default function RoadMapCards() {
     return (
         <>
-            <div className="max-w-screen-xl bg-red-400 flex items-center justify-center gap-8">
+            <div className="max-w-screen-xl flex items-center justify-center gap-8 mt-24">
                 {CardData.map((data) => (
                     <div
                         key={data.id}
                         title="container"
-                        className="w-72 h-96 rounded-2xl bg-slate-800 text-white font-jet">
+                        className="w-72 h-auto rounded-2xl bg-slate-800 text-white font-jet">
                         <img
                             src={data.image}
                             alt=""
                             className="w-64 h-40 object-cover mx-auto mt-4 rounded-xl"
                         />
-                        <div className="w-64 mx-auto mt-3 bg-green-600">
-                            <h2 className="text-white font-medium">{data.title}</h2>
+                        <div className="w-64  mt-3 text-lg flex items-center mx-auto">
+                            <h2 className="text-white font-medium">
+                                {data.title}
+                            </h2>
+                            <HeartIcon className="size-6 text-or-300 ml-auto"/>
                         </div>
-                        <EnRollBtn/>
+                        <div className="w-64 mx-auto ">
+                            <h2 className="text-or-300 font-medium mt-10">
+                                By:{" "}
+                                <span className="text-white text-lg">{data.by}</span>
+                            </h2>
+                        </div>
+                        <div className="my-3">
+                            <EnRollBtn />
+                        </div>
                     </div>
                 ))}
             </div>
